ece8dcc20da3b013a48720f25557f1a9c6a97a69
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 11:37:52 +0530

    code completed

--- CountDigitsAndAlphabets/Program.cs
+++ CountDigitsAndAlphabets/Program.cs
@@ -6,7 +6,26 @@ namespace CountDigitsAndAlphabets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str;
+            int alphabet, digit, i, l;
+            alphabet = digit = i = l = 0;
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+            l = str.Length;
+
+            while (i < l)
+            {
+                if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) 
+                {
+                    alphabet++;
+                } else if(str[i] >='0' && str[i] <='9')
+                {
+                    digit++;
+                }
+                i++;
+            }
+            Console.WriteLine("Alphabets: {0}", alphabet);
+            Console.WriteLine("Digits: {0}", digit);
         }
     }
 }
